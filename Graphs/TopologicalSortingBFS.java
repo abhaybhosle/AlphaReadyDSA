cefe998644199017e5dbce19fa017e5222a7de28
abhayBhosle <abhaybhosle699@gmail.com>
2023-09-11 19:06:32 +0530

    TopSortBFS

---
+++ Graphs/TopologicalSortingBFS.java
@@ -0,0 +1,77 @@
+package Graphs;
+
+import java.util.*;
+
+public class TopologicalSortingBFS {
+
+    static class Edge {
+        int src;
+        int dest;
+
+        Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[2].add(new Edge(2, 3));
+        graph[3].add(new Edge(3, 1)); // Corrected source and destination
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+
+    public static void calculateIndegree(ArrayList<Edge> graph[], int indeg[]) { // this function calculates indegree
+                                                                                 // for
+                                                                                 // all nodes
+        for (int i = 0; i < graph.length; i++) {
+            int v = i;
+            for (int j = 0; j < graph[v].size(); i++) {
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSort(ArrayList<Edge>[] graph) {
+        int indeg[] = new int[graph.length]; // indegree array
+        calculateIndegree(graph, indeg); // it will calculate all the indegrees for all nodes
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i); // First add only those node who have indegree count as 0 to the queue
+            }
+        }
+        // bfs
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " "); // topological sorting print
+
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--; // decrease the count for the neighbour,if it equal to 0 , add to queue
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+
+            }
+
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        topSort(graph);
+
+    }
+}
