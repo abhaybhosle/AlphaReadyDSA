b37047b0466e6ccae8d5ab6f87c445452ba12936
abhayBhosle <abhaybhosle699@gmail.com>
2023-09-12 19:49:02 +0530

    DijkstrasAlgorithm

---
+++ Graphs/DijkstrasAlgoritm.java
@@ -0,0 +1,101 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class DijkstrasAlgoritm {
+
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
+
+        graph[2].add(new Edge(2, 4, 3));
+
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int n;// node
+        int path;// distance
+
+        Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.path - p2.path; // path based sorting for my pairs. Shortest distance from node 0 to neighbours
+                                        // has highest priority
+                                        // by default it sorts in increasing order
+        }
+
+    }
+    //TC = V+ElogV
+    public static void dijkstras(ArrayList<Edge> graph[], int src) {
+        int distance[] = new int[graph.length];// dist[i] -> src to i
+        for (int i = 0; i < graph.length; i++) {
+            if (i != src) {
+                distance[i] = Integer.MAX_VALUE; // +infinity
+            }
+        }
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+        // loop
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+                // neighbours
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if (distance[u] + wt < distance[v]) { // update distance of src to v
+                        distance[v] = distance[u] + wt; //// formula for dijkstras algorithm
+                        pq.add(new Pair(v, distance[v]));
+                    }
+                }
+            }
+        }
+        // print all source to vertices shortest distance
+        for (int i = 0; i < distance.length; i++) {
+            System.out.print(distance[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        int src = 0;
+        dijkstras(graph, src);
+
+    }
+}
